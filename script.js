6dfa2ea35d5fa8fe19b48834d7d5b34a5ef40137
Luke Moody [LMWD] <hello@lukemoody.co.uk>
2019-02-22 21:33:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Create new XMLHttpRequest
 var request = new XMLHttpRequest(),
-            method = "GET",
-            url = "xxxx"; // Add API url
+              method = "GET",
+              url = "xxxx"; // Add API url
 
-        request.open(method, url, true);
+request.open(method, url, true);
 
-        request.onload = function () {
-          // begin accessing JSON data here
-          var data = JSON.parse(this.response);
+request.onload = function () {
+  // begin accessing JSON data here
+  var data = JSON.parse(this.response);
 
-          var listItems = [],
-              n;
+  var listItems = [],
+       n;
 
-          // Loop through available data fetched from api
-          for (var i = 0; i < data.length; i++) {
+  // Loop through available data fetched from api
+  for (var i = 0; i < data.length; i++) {
 
-            console.log( data[i].name );
-            n = '<h3>' + data[i].name + '</h3>';
+    console.log( data[i].name );
+    n = '<h3>' + data[i].name + '</h3>';
 
-            listItems.push( n );
-          }
-          document.getElementById("demo").innerHTML = listItems.join("");
-        }
-        request.send();
+    listItems.push( n );
+  }
+  document.getElementById("demo").innerHTML = listItems.join("");
+}
+request.send();
